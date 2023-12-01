8cab9e2f3127ee79f1bebf6335ef8a5f26adfbe4
yav <796176@protonmail.com>
2023-12-01 18:15:53 +0300

    Overload MazeGenerator constructor

--- src/main/java/Maze/Generator/MazeGenerator.java
+++ src/main/java/Maze/Generator/MazeGenerator.java
@@ -1,4 +1,12 @@
 package Maze.Generator;
 
 public class MazeGenerator implements MazeGeneratorInterface{
+	private int mazeWidth;
+	private int mazeHeight;
+	private int mazeDifficulty;
+	MazeGenerator(int width, int height, int difficulty){
+		mazeHeight = height;
+		mazeWidth = width;
+		mazeDifficulty = difficulty;
+	}
 }
