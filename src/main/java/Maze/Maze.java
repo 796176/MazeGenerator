bedc5e90e804c7f0fd932bd1c1c2b264c1cdfdd6
yav <796176@protonmail.com>
2023-12-01 16:00:53 +0300

    Add Maze class

---
+++ src/main/java/Maze/Maze.java
@@ -0,0 +1,15 @@
+package Maze;
+public class Maze {
+	private int[][] mazeArray;
+	private int height;
+	private int width;
+	private Crotch root;
+
+	public int getHeight() {
+		return height;
+	}
+
+	public int getWidth(){
+		return width;
+	}
+}
