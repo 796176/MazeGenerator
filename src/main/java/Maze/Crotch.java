2920ec7a796574017fb4b49ad1686bc51e910b6d
yav <796176@protonmail.com>
2023-12-01 16:00:34 +0300

    Add Crotch class

---
+++ src/main/java/Maze/Crotch.java
@@ -0,0 +1,12 @@
+package Maze;
+
+public class Crotch {
+	public Crotch lCrotch;
+	public Crotch cCrotch;
+	public Crotch rCrotch;
+
+	public int[] startPosition = new int[2];
+	public int[] endPosition = new int[2];
+
+	public int length;
+}
