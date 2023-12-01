9360aa05c249d0c1179d1e8cd503dd2b9f2bbc52
yav <796176@protonmail.com>
2023-12-01 18:10:07 +0300

    Add public methods into MazeGeneratorInterface

--- src/main/java/Maze/Generator/MazeGeneratorInterface.java
+++ src/main/java/Maze/Generator/MazeGeneratorInterface.java
@@ -1,4 +1,12 @@
 package Maze.Generator;
+import Maze.*;
 
 public interface MazeGeneratorInterface {
+	public Maze generate();
+	public int getHeight();
+	public int getWidth();
+	public int getDifficulty();
+	public void setHeight(int height);
+	public void setWidth(int width);
+	public void setDifficulty(int difficulty);
 }
