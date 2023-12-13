75a83dcc37720869c035b682a5497c93eab459aa
yav <796176@protonmail.com>
2023-12-13 17:20:20 +0300

    Add extra variables in Crotch

--- src/main/java/Maze/Crotch.java
+++ src/main/java/Maze/Crotch.java
@@ -6,7 +6,12 @@ public class Crotch {
 	public Crotch rCrotch;
 
 	public int[] startPosition = new int[2];
+	public int startPositionX;
+	public int startPositionY;
+	public int endPositionX;
+	public int endPositionY;
 	public int[] endPosition = new int[2];
 
 	public int length;
+	public int direction;
 }
