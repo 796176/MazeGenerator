b6cf9bb0cffe0c4d019934a870704fcce38e3099
yav <796176@protonmail.com>
2023-12-13 17:18:22 +0300

    Overload Maze constructor

--- src/main/java/Maze/Maze.java
+++ src/main/java/Maze/Maze.java
@@ -1,5 +1,10 @@
 package Maze;
 public class Maze {
+	public Maze(int height, int width, Crotch root){
+		this.height = height;
+		this.width = width;
+		this.root = root;
+	}
 	private int[][] mazeArray;
 	private int height;
 	private int width;
