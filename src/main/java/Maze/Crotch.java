efdd2f40305b6fd18ebad012eeeea8c3593b1fa1
yav <796176@protonmail.com>
2023-12-13 17:22:05 +0300

    Add global direction variables in Crotch

--- src/main/java/Maze/Crotch.java
+++ src/main/java/Maze/Crotch.java
@@ -1,6 +1,10 @@
 package Maze;
 
 public class Crotch {
+	public static final int LEFT_TO_RIGHT = 0;
+	public static final int RIGHT_TO_LEFT = 1;
+	public static final int DOWNWARD = 2;
+	public static final int UPWARD = 3;
 	public Crotch lCrotch;
 	public Crotch cCrotch;
 	public Crotch rCrotch;
