8e17bb663ae5b279a1ced19084d0b5d2cb40a288
yav <796176@protonmail.com>
2023-12-15 15:06:23 +0300

    Add a new type of storage into Maze
    Calling the Maze construction performs the translation from the tree data structure
    into two-dimensional array

--- src/main/java/Maze/Maze.java
+++ src/main/java/Maze/Maze.java
@@ -4,6 +4,8 @@ public class Maze {
 		this.height = height;
 		this.width = width;
 		this.root = root;
+		mazeArray = new int[height][width];
+		arrayStorageTypeTranslation(root);
 	}
 	private int[][] mazeArray;
 	private int height;
@@ -16,5 +18,34 @@ public class Maze {
 
 	public int getWidth(){
 		return width;
+	}
+
+	private void arrayStorageTypeTranslation(Crotch c){
+		switch (c.direction) {
+			case Crotch.LEFT_TO_RIGHT:
+				for (int i = c.startPositionX; i <= c.endPositionX; i++) {
+					mazeArray[c.startPositionY][i] = 1;
+				}
+				break;
+			case Crotch.RIGHT_TO_LEFT:
+				for (int i = c.endPositionX; i <= c.startPositionX; i++) {
+					mazeArray[c.startPositionY][i] = 1;
+				}
+				break;
+			case Crotch.DOWNWARD:
+				for (int i = c.startPositionY; i <= c.endPositionY; i++) {
+					mazeArray[i][c.startPositionX] = 1;
+				}
+				break;
+			case Crotch.UPWARD:
+				for (int i = c.endPositionY; i <= c.startPositionY; i++) {
+					mazeArray[i][c.startPositionX] = 1;
+				}
+				break;
+		}
+
+		if (c.lCrotch != null) arrayStorageTypeTranslation(c.lCrotch);
+		if (c.cCrotch != null) arrayStorageTypeTranslation(c.cCrotch);
+		if (c.rCrotch != null) arrayStorageTypeTranslation(c.rCrotch);
 	}
 }
